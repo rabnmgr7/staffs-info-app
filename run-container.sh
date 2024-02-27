13ec86f63e96acf99c5c2baec47f099000b7e864
rabnmgr7 <rabnmgr7@gmail.com>
2024-02-27 05:12:08 +0000

    restart policy changed

--- run-container.sh
+++ run-container.sh
@@ -7,4 +7,4 @@ echo "Creating network"
 docker network create --subnet 10.0.10.0/24 --gateway 10.0.10.1 --driver=bridge staffs-info-net || true
 echo "Creating containers"
 docker container run -itd --name staffs-info-db-cont --network staffs-info-net --ip 10.0.10.2 --restart always -p 3318:3306 staffs-info-db:latest
-docker container run -itd --name staffs-info-node-app-cont --network staffs-info-net --ip 10.0.10.3 -v ./node/api:/app/api -p 8081:8123 staffs-info-node-app:latest
+docker container run -itd --name staffs-info-node-app-cont --network staffs-info-net --ip 10.0.10.3 --restart always -v ./node/api:/app/api -p 8081:8123 staffs-info-node-app:latest
