f50335661c4e8b2a98cd6f93f03523b28e5ade80
rabnmgr7 <98509375+rabnmgr7@users.noreply.github.com>
2024-03-08 16:37:40 +0545

    Update img-build.sh

--- img-build.sh
+++ img-build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker image rm -f staffs-info-node-app:latest staffs-info-db:latest || true
-docker build -t staffs-info-node-app ./node/
-docker build -t staffs-info-db ./db/
+docker image rm -f harbor.registry.local/staffs-info-app/node:latest harbor.registry.local/staffs-info-app/db:latest || true
+docker build -t harbor.registry.local/staffs-info-app/node:latest ./node/
+docker build -t harbor.registry.local/staffs-info-app/db:latest ./db/
